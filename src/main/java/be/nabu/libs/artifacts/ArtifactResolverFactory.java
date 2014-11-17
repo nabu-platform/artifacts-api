54e7a18f246588f80c3d560f619e824d0c46b830
alex <alex@nabu.be>
2014-11-17 15:43:10 +0100

    Updated classloader scope for service loading

--- src/main/java/be/nabu/libs/artifacts/ArtifactResolverFactory.java
+++ src/main/java/be/nabu/libs/artifacts/ArtifactResolverFactory.java
@@ -25,7 +25,7 @@ public class ArtifactResolverFactory {
 		if (resolvers.isEmpty()) {
 			try {
 				// let's try this with custom service loading based on a configuration
-				Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
+				Class<?> clazz = getClass().getClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
 				Method declaredMethod = clazz.getDeclaredMethod("load", Class.class);
 				resolvers.addAll((List<ArtifactResolver<?>>) declaredMethod.invoke(null, ArtifactResolver.class));
 			}
