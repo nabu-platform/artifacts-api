eb327d2d27dbee83a01a0287566dfc784ce1e546
Alexander Verbruggen <alex@nabu.be>
2018-05-28 17:44:16 +0200

    Added context support for data source providers

--- src/main/java/be/nabu/libs/artifacts/api/DataSourceProviderArtifact.java
+++ src/main/java/be/nabu/libs/artifacts/api/DataSourceProviderArtifact.java
@@ -7,4 +7,7 @@ import be.nabu.libs.artifacts.api.Artifact;
 public interface DataSourceProviderArtifact extends Artifact {
 	public DataSource getDataSource();
 	public boolean isAutoCommit();
+	// the context for the data source provider
+	// based on the context we can do better matching for which provider to use when
+	public default String getContext() { return null; }
 }
